f164387e76fb757b15c60cd8fc8fc23b7211f771
Justin Jordan <yyttrium3989@gmail.com>
2022-12-08 10:31:20 -0500

    added filter bar

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="home">
-    <p>Tap on an event to see more details</p>
-    <event-list />
+  <div class="home_page">
+    <p class="home" id="home_header">Tap on an event to see more details</p>
+    <div class="home" id="home_filter">
+      <p id="filter_header">Filter</p>
+      <form>
+        <label for="zipcode" id="filter_zip">Zip Code: </label>
+        <input type="text" id="filter_zip" name="filter_zip" maxlength="5">&nbsp;
+        <input type="submit" value="Submit">
+      </form>
+    </div>
+    <event-list class="home" id="home_events" />
   </div>
 </template>
 
@@ -11,7 +19,44 @@ import EventList from "../components/Events"
 export default {
   name: "home",
   components: {
-    EventList
+    EventList,
   }
 };
 </script>
+
+<style>
+.home_page{
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+  grid-template-areas: 
+    "header header"
+    "filter events";
+}
+
+#home_header{
+  grid-area: header;
+}
+
+#home_filter{
+  grid-area: filter;
+  background-color: #7ebdc2;
+  margin: 8px;
+  padding: 8px;
+  border-radius: 12px;
+}
+
+#filter_header{
+  font-size: x-large;
+  font-weight: bold;
+}
+
+#filter_zip{
+  font-size: large;
+  font-weight: bold;
+  width: 45%;
+}
+
+#home_events{
+  grid-area: events;
+}
+</style>
